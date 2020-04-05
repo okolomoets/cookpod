b858aae0b4e8d11dc94a6ad895e70fa0d0cb3603
{()}JIer <kolomoets.oleg@gmail.com>
2020-04-06 00:08:17 +0300

    Add credo to the application. Add credo to CI tasks list.

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Cookpod.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:excoveralls, "~> 0.12.3"}
+      {:excoveralls, "~> 0.12.3"},
+      {:credo, "~> 1.3", only: [:dev, :test], runtime: false}
     ]
   end
 
