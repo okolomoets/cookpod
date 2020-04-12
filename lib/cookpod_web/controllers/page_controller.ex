0655a3dad5a029b626743deea7fd57ffa790af70
{()}JIer <kolomoets.oleg@gmail.com>
2020-04-13 01:15:59 +0300

    Add :show and :delete actions to the session controller. Add template for show action. Add plug for Auth check.

--- lib/cookpod_web/controllers/page_controller.ex
+++ lib/cookpod_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule CookpodWeb.PageController do
   use CookpodWeb, :controller
 
+  plug CookpodWeb.AuthPlug when action in [:terms]
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
--- lib/cookpod_web/controllers/session_controller.ex
+++ lib/cookpod_web/controllers/session_controller.ex
@@ -1,19 +1,30 @@
 defmodule CookpodWeb.SessionController do
   use CookpodWeb, :controller
 
+  def show(conn, _params) do
+    current_user = get_session(conn, :current_user)
+    render(conn, "show.html", current_user: current_user)
+  end
+
   def new(conn, _params) do
     render(conn, "new.html", errors: %{})
   end
 
-  def create(conn, %{"user" => user}) do 
-    case validate_user(user) do 
-      errors when map_size(errors) == 0 -> 
+  def create(conn, %{"user" => user}) do
+    case validate_user(user) do
+      errors when map_size(errors) == 0 ->
         conn
         |> put_session(:current_user, user["name"])
         |> redirect(to: Routes.page_path(conn, :index))
-      errors -> 
+      errors ->
         render(conn, "new.html", errors: errors)
     end
+  end
+
+  def delete(conn, _params) do
+    conn
+    |> delete_session(:current_user)
+    |> redirect(to: Routes.page_path(conn, :index))
   end
 
   defp validate_user(user) do
