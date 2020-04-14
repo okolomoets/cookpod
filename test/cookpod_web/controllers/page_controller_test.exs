1c883deb93febd851b26c903bb75990910f153cc
{()}JIer <kolomoets.oleg@gmail.com>
2020-04-14 21:07:57 +0300

    Fix lint issue

--- test/cookpod_web/controllers/page_controller_test.exs
+++ test/cookpod_web/controllers/page_controller_test.exs
@@ -6,7 +6,7 @@ defmodule CookpodWeb.PageControllerTest do
     conn = conn
       |> init_test_session(%{current_user: "test"})
       |> put_req_header("authorization", "Basic " <> Base.encode64("admin:some_pass"))
-      |> get( "/")
+      |> get("/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
