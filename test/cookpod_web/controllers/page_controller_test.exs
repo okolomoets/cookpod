c3d3bd95c925dc28cd4da0b45cdbd9e59efd6bc1
{()}JIer <kolomoets.oleg@gmail.com>
2020-04-27 00:54:51 +0300

    Fix specs

--- test/cookpod_web/controllers/page_controller_test.exs
+++ test/cookpod_web/controllers/page_controller_test.exs
@@ -1,12 +1,15 @@
 defmodule CookpodWeb.PageControllerTest do
   use CookpodWeb.ConnCase
   import Plug.Test
+  alias Cookpod.User
 
   test "GET /", %{conn: conn} do
+    user = %User{email: "test@gmail.com"}
     conn = conn
-      |> init_test_session(%{current_user: "test"})
-      |> put_req_header("authorization", "Basic " <> Base.encode64("admin:some_pass"))
-      |> get("/")
+    |> init_test_session(%{current_user: user})
+    |> put_req_header("authorization", "Basic " <> Base.encode64("admin:some_pass"))
+    |> get(Routes.page_path(conn, :index))
+
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
